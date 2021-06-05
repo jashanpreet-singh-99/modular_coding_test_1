bec425d0bf7178119bd1e9b8b21db51d2aabcd9e
Jashanpreet Singh <jashan.singh.work@icloud.com>
2021-06-06 00:05:34 +0530

    Added adding_child ability. Initial phase.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from tree import Tree
 
 PATH_SRC = os.path.join(os.getcwd(), "src")
 PATH_OUT = os.path.join(os.getcwd(), "out")
@@ -9,3 +10,10 @@ PATH_OUT = os.path.join(os.getcwd(), "out")
 def compare_files():
     missing_files = []
     extra_files = []
+
+
+directory_tree = Tree("root_node")
+directory_tree.add_child(directory_tree.root, "node_L")
+directory_tree.add_child(directory_tree.root, "node_M")
+directory_tree.add_child(directory_tree.root, "node_R")
+print(directory_tree)
--- tree.py
+++ tree.py
@@ -1,6 +1,14 @@
 class Tree:
     def __init__(self, root):
         self.root = root
+        self.children = {}
 
     def __str__(self):
         return f'Root -> {self.root}'
+
+    def add_child(self, parent, child):
+        if self.root not in self.children.keys() or \
+                len(self.children[parent]) == 0:
+            self.children[parent] = [child]
+        else:
+            self.children[parent].append(child)
