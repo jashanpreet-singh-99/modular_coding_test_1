92731bd5f393780be0356c036c23d827b799b6cf
Jashanpreet Singh <jashan.singh.work@icloud.com>
2021-07-02 22:53:33 +0530

    Improved output readability and removed some boilerplate comments.

--- tree.py
+++ tree.py
@@ -16,37 +16,6 @@ class Tree:
                     " type Node."
             raise Node.NodeValueError(message)
 
-    # def update_tree(self):
-    #     self.level_val = {}
-    #     self.level_val[0] = [self.root.value]
-    #     level_count = 1
-    #     parent = self.root
-    #     prev_parent = None
-    #     child_count = 0
-    #     while True:
-    #         if len(parent.child) > 0:
-    #             for child in parent.child:
-    #                 if level_count not in self.level_val.keys():
-    #                     self.level_val[level_count] = []
-    #                 if not isinstance(child, Node):
-    #                   message = f"Variable : child({type(child)}) of parent"\
-    #                             f" : {parent.value} -> is not of type Node."
-    #                     raise Node.NodeValueError(message)
-    #                 self.level_val[level_count].append(child.value)
-    #             if (len(self.level_val[level_count - 1]) - 1) == child_count:
-    #                 prev_parent = parent
-    #                 parent = parent.child[0]
-    #                 level_count += 1
-    #                 child_count = 0
-    #             else:
-    #                 break
-    #         else:
-    #             if (len(self.level_val[level_count - 1]) - 1) != child_count:
-    #                 child_count += 1
-    #                 parent = prev_parent.child[child_count]
-    #             else:
-    #                 break
-
     def update_tree(self):
         self.tree_data = {}
         self.traverse_tree(self.root, 'L0')
@@ -63,13 +32,6 @@ class Tree:
         print(len(self.tree_data.keys()))
         return self.tree_data.keys()
 
-    # def __str__(self):
-    #     self.update_tree()
-    #     return_string = 'TREE : \n'
-    #     for k, v in self.level_val.items():
-    #         return_string += f' {k} : {self.level_val[k]}\n'
-    #     return return_string
-
     def __str__(self):
         self.update_tree()
         return_string = 'TREE : \n'
@@ -77,7 +39,7 @@ class Tree:
             if v[2] == '.':
                 continue
             level = len(k.split('_')) - 1
-            return_string += '  ' * level + v + '\n'
+            return_string += '|-' + '---' * level + v + '\n'
             continue
         return return_string
 
